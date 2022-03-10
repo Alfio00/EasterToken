3caa6e2290e874163aec24986916e9d563d1e563
Alfio00 <supermediatablet@gmail.com>
2022-03-10 23:41:19 +0100

    hotfix 1.2

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8935dbe0e368ba04d721";
+/******/ 	var hotCurrentHash = "028514b84f735883d2dd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
