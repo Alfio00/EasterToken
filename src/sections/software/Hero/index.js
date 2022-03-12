6dd480886f6de136e6b5c9503a28de23aa3d3132
Alfio00 <supermediatablet@gmail.com>
2022-03-12 18:57:56 +0100

    hotfix 1.6

--- src/sections/software/Hero/index.js
+++ src/sections/software/Hero/index.js
@@ -12,7 +12,7 @@ export default function HeroSection ({data}) {
        <div className="overlay">
         <h1>OUR BUNNY LOST ALL HIS EGGS</h1>
         <h3>HODL and help him catch'em all!</h3>
-         <button src="bunnycoinbsc.com/easteregg.html" type="button" class="btn41-43 btn-42" style={{width: '166px', fontWeight: '900'}}>FIND YOUR EGG</button>
+         <button onClick={"bunnycoinbsc.com/easteregg.html"} type="button" class="btn41-43 btn-42" style={{width: '166px', fontWeight: '900'}}>FIND YOUR EGG</button>
        </div>
     </div>
   )
