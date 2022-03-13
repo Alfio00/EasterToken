b86e0f49e689f130ea9debe668577a45d760b6e8
Alfio00 <supermediatablet@gmail.com>
2022-03-13 01:11:01 +0100

    hotfix 1.7

--- src/data/menudata.js
+++ src/data/menudata.js
@@ -2,7 +2,7 @@
 
 export const menuItems = [
          {
-           name: "bunnycoinbsc.com/easteregg.html",
+           name: "/easteregg.html",
            label: "Easter Egg",
          },
            {
